8208cfaaa71d0dc16676cb27ea551e4acd60a561
Emilio Islas <milio11@yahoo.com>
2016-12-26 03:37:04 -0800

    test

--- js/require.js
+++ js/require.js
@@ -174,7 +174,8 @@ function require(identifier, callback, compiler) {
 	//      seems to have an issue where onreadystatechange is called twice for
 	//      readyState 4.
 	callback && (request[request.onload===null?'onload':'onreadystatechange'] = onLoad);
-	descriptor.uri = descriptor.uri.replace('#/./', ''); 
+	descriptor.uri = descriptor.uri.replace('#/./', '');
+	console.log(descriptor.uri);
 	request.open('GET', descriptor.uri, !!callback);
 	lock[cacheid] = lock[cacheid]++||1;
 	request.send();
