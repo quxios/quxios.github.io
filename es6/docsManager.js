c79e5580be158d4b98e33933b8e30dd1dc3d5ced
Emilio Islas <exinyo@gmail.com>
2017-02-12 01:40:36 -0800

    test

--- es6/docsManager.js
+++ es6/docsManager.js
@@ -2,12 +2,14 @@ export default class PluginManager {
   static docsPath = '/data/docs/';
 
   static load(name, callback) {
+    console.log('loading', name);
     let xhr = new XMLHttpRequest();
     xhr.overrideMimeType("application/json");
     xhr.open('GET', this.docsPath + name + '.json', true);
     xhr.onreadystatechange = () => {
       if (xhr.readyState == 4 && xhr.status == '200') {
         if (callback) {
+          console.log('loaded');
           let obj = JSON.parse(xhr.responseText);
           callback(obj);
         }
